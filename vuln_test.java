2ff51ced5716df873306e8467157e78f4d8ccffa
Zanis Ali Khan <zanisali999@gmail.com>
2025-04-09 14:17:47 +0200

    Update vuln_test.java

--- vuln_test.java
+++ vuln_test.java
@@ -1,9 +1,9 @@
 public class VulnerableFunctions {
 
-    //  A function that copies data into a fixed-size buffer without checking the length of the input.
-    public void copyString(String input) {
-        byte[] buffer = new byte[64];
-        System.arraycopy(input.getBytes(), 0, buffer, 0, input.length());
+    // use of hard coded credentials
+    public void connectToDatabase() {
+        String username = "admin";
+        String password = "password123";
     }
 
 
